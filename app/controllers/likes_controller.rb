22082ca1231abdaad92b9a8318088a00e7fb464f
Ryan Xin <xinpf0715@gmail.com>
2020-08-10 17:41:17 +1000

    undo delete by mistake like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,47 @@
 class LikesController < ApplicationController
-  def create
+
+  def new
   end
+
+  def create
+    @post = Post.find params[:post_id]
+    @post.likes.create(user_id: @current_user.id)
+    # HTTP referral header: the previous visted URL
+    url = Rails.application.routes.recognize_path(request.referer)
+    last_action = url[:action]
+    # Comment on show page stay at show page
+    if last_action == "show"
+      redirect_to(request.referer + '#start')     
+      return
+    end      
+    # Comment on index page stay at index page
+    redirect_to(request.referer + '#' + params[:post_id])
+  end # create
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    Like.destroy params[:id]
+    # HTTP referral header: the previous visted URL
+    url = Rails.application.routes.recognize_path(request.referer)
+    last_action = url[:action]
+    # Comment on show page stay at show page
+    if last_action == "show"
+      redirect_to(request.referer + '#start')     
+      return
+    end      
+    # Comment on index page stay at index page
+    redirect_to(request.referer + '#' + params[:post_id])
+  end
+
 end

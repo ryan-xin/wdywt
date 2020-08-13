e5f7122a5b5250909db5ff0fe75766c174bd84af
Ryan Xin <xinpf0715@gmail.com>
2020-08-13 11:53:45 +1000

    follow unfollow function done

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -3,7 +3,6 @@ class FollowsController < ApplicationController
   end
 
   def create
-    raise "hell"
     @user = User.find params[:user_id]
     @current_user.following << @user
     redirect_to(request.referer)
@@ -22,5 +21,16 @@ class FollowsController < ApplicationController
   end
 
   def destroy
+    Follow.destroy params[:id]
+    # HTTP referral header: the previous visted URL
+    url = Rails.application.routes.recognize_path(request.referer)
+    last_action = url[:action]
+    # Comment on show page stay at show page
+    if last_action == "show"
+      redirect_to(request.referer + '#follow')     
+      return
+    end      
+    # Comment on index page stay at index page
+    redirect_to(request.referer)   
   end
 end

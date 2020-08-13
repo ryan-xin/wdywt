52043922d9cff3e5696dd59569c40b7e534127af
Ryan Xin <xinpf0715@gmail.com>
2020-08-13 15:21:02 +1000

    change tab_id to tabId in jQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,13 +19,13 @@ $(document).ready(function () {
   console.log("jQuery")
 
   $('.user_center_tab ul li').click(function () {
-    let tab_id = $(this).attr('tab-number');
+    let tabId = $(this).attr('tab_number');
 
     $('.user_center_tab ul li').removeClass('link_current');
     $('.tab_content').removeClass('tab_current');
 
     $(this).addClass('link_current')
-    $('#' + tab_id).addClass('tab_current');
+    $('#' + tabId).addClass('tab_current');
 
   })
 

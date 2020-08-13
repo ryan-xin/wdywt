b23eb1d97efa745a18c03ec0b56dbb3f6368ae64
Ryan Xin <xinpf0715@gmail.com>
2020-08-13 16:03:03 +1000

    change explore to following on nav

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,11 +6,20 @@ class PagesController < ApplicationController
   end
 
   def home
+    # @posts = Post.all.order("created_at DESC")
+    # @comment = Comment.new
+    # unless @current_user.following.exists?
+    #   redirect_to(posts_path)
+    # end
+    redirect_to(posts_path)
+  end
+
+  def following
     @posts = Post.all.order("created_at DESC")
     @comment = Comment.new
     unless @current_user.following.exists?
       redirect_to(posts_path)
-    end
+    end    
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "pages#home"
+  get "/following" => "pages#following"
   # get "/home" => "pages#home"
 
   # Session Routes -------------------------------------------
